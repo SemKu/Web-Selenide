ff3d87ddf0c6ca7a0ed26b8cb5db972c172a0517
Semen Kuzmin <eurosemen@yandex.ru>
2022-05-12 14:52:06 +0300

    added tests

--- src/test/java/ru/netology/web/SelenideTest.java
+++ src/test/java/ru/netology/web/SelenideTest.java
@@ -3,7 +3,6 @@ package ru.netology.web;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -15,16 +14,38 @@ public class SelenideTest {
         $("[data-test-id=phone] input.input__control").setValue("+79111133030");
         $("[data-test-id=agreement]").click();
         $(".button__content").click();
-        $(withText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."))
+        $("[data-test-id=order-success]")
                 .shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldNotCompletedNameFieldValidation() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input.input__control").setValue("");
+        $("[data-test-id=phone] input.input__control").setValue("+79111133030");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=name] .input__sub").should(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldNotCompletedTelFieldValidation() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input.input__control").setValue("Семен Кузьмин");
+        $("[data-test-id=phone] input.input__control").setValue("");
+        $("[data-test-id=agreement]").click();
+        $(".button__content").click();
+        $("[data-test-id=phone] .input__sub").should(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldNameFieldValidation() {
         open("http://localhost:9999");
         $("[data-test-id=name] input.input__control").setValue("Semen Kuz");
+        $("[data-test-id=phone] input.input__control").setValue("+79111133030");
+        $("[data-test-id=agreement]").click();
         $(".button__content").click();
-        $(withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."))
+        $("[data-test-id=name] .input__sub")
                 .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
@@ -34,7 +55,7 @@ public class SelenideTest {
         $("[data-test-id=name] input.input__control").setValue("Семен Кузьмин");
         $("[data-test-id=phone] input.input__control").setValue("9111133030");
         $(".button__content").click();
-        $(withText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."))
+        $("[data-test-id=phone] .input__sub")
                 .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
@@ -44,7 +65,7 @@ public class SelenideTest {
         $("[data-test-id=name] input.input__control").setValue("Семен Кузьмин");
         $("[data-test-id=phone] input.input__control").setValue("+79111133030");
         $(".button__content").click();
-        $(".input_invalid")
+        $("[data-test-id=agreement].input_invalid")
                 .shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных" +
                         " и разрешаю сделать запрос в бюро кредитных историй"));
     }
